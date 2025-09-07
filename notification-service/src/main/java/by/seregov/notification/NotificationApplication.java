516ce769e121697eec17b04e2aac55f6367e081a
Sergey Seregov <seregov.sd@yandex.by>
2025-09-08 02:28:38 +0300

    refactor: rename Application classes and add @EnableDiscoveryClient

--- notification-service/src/main/java/by/seregov/notification/NotificationApplication.java
+++ notification-service/src/main/java/by/seregov/notification/NotificationApplication.java
@@ -3,10 +3,12 @@ package by.seregov.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+public class NotificationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(NotificationApplication.class, args);
     }
 }
--- user-service/src/main/java/by/task/userservice/UserApplication.java
+++ user-service/src/main/java/by/task/userservice/UserApplication.java
@@ -2,10 +2,12 @@ package by.task.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 }
